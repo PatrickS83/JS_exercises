f321a185cfeb1e4ea2d6f008aba393961af959d2
Patrick Schuelke <p-schuelke@gmx.de>
2017-11-08 20:21:10 +0100

    Add objects instead of strings and toggle function

--- TodoList/todo.js
+++ TodoList/todo.js
@@ -1,22 +1,30 @@
 var todoList = {
-  todos: ['todo 1', 'todo 2', 'todo 3'],
+  todos: [],
   // Function to display ToDo Items
   displayTodos: function() {
     console.log('Stuff to do', this.todos);
   },
-  //Function to add ToDo Items
-  addTodo: function(todo) {
-    this.todos.push(todo);
+  //Function to add ToDo Items as objects
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
     this.displayTodos();
   },
   //Function to change ToDo Items
-  changeTodo: function(position, newValue) {
-    this.todos(position) = newValue;
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText;
     this.displayTodos();
   },
   //Function to delete ToDo Items
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function(position) {
+    let todo = this.todos[position];
+    todo.completed = !todo.completed;
     this.displayTodos();
   }
 };
