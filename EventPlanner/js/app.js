bee59b4bcf0f7b3fe142e3c4c6a0095084fce1e8
PatrickSchuelke <p-schuelke@gmx.de>
2017-12-14 14:15:24 +0100

    add eventlistener for delete btn

--- EventPlanner/js/app.js
+++ EventPlanner/js/app.js
@@ -2,3 +2,27 @@
 $(document).ready(function() {
   $("select").material_select();
 });
+
+const form = document.querySelector("#form"),
+  inputs = document.querySelectorAll("input");
+
+const party = {
+  guests: [],
+
+  addGuest() {}
+};
+
+const ui = {
+  setupEventListener() {
+    const table = document.querySelector("table");
+    table.addEventListener("click", e => {
+      const elementClicked = e.target;
+      if (elementClicked.id === "delete") {
+        console.log("yes");
+      }
+      e.preventDefault();
+    });
+  }
+};
+
+ui.setupEventListener();
