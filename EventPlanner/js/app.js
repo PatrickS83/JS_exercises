bb2a195ceca3e7e01988f88f621efbb62aeba4b2
PatrickSchuelke <p-schuelke@gmx.de>
2017-12-14 14:55:58 +0100

    added deleteGuest function

--- EventPlanner/js/app.js
+++ EventPlanner/js/app.js
@@ -21,17 +21,45 @@ const party = {
     };
     if (!!guest.firstName && !!guest.lastName && !!guest.bringItem) {
       this.guests.push(guest);
+      ui.displayGuests();
     }
+  },
+
+  deleteGuest(guestID) {
+    this.guests.splice(guestID, 1);
+    ui.displayGuests();
   }
 };
 
 const ui = {
+  displayGuests() {
+    const tableBody = document.querySelector("tbody");
+    tableBody.innerHTML = "";
+    party.guests.forEach((guest, index) => {
+      const row = document.createElement("tr");
+      row.id = index;
+      row.innerHTML = `
+        <td>${guest.firstName} ${guest.lastName}</td>
+        <td>${guest.plusOne}</td>
+        <td>${guest.email}</td>
+        <td>${guest.bringItem}</td>
+        <td>
+          <a class="btn-floating waves-effect waves-light red">
+            <i id="delete" class="material-icons">clear</i>
+          </a>
+        </td>
+        `;
+      tableBody.appendChild(row);
+    });
+  },
+
   setupEventListener() {
     const table = document.querySelector("table");
     table.addEventListener("click", e => {
       const elementClicked = e.target;
       if (elementClicked.id === "delete") {
-        console.log("yes");
+        const guestID = elementClicked.parentElement.parentElement.parentElement.id;
+        party.deleteGuest(guestID);
       }
       e.preventDefault();
     });
