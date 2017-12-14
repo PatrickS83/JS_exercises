4bc7ee88af01aabe01a366e4c909e5e2736a7ec1
PatrickSchuelke <p-schuelke@gmx.de>
2017-12-14 14:36:20 +0100

    add addGuest function

--- EventPlanner/js/app.js
+++ EventPlanner/js/app.js
@@ -3,13 +3,26 @@ $(document).ready(function() {
   $("select").material_select();
 });
 
-const form = document.querySelector("#form"),
-  inputs = document.querySelectorAll("input");
+const form = document.querySelector("#form");
 
 const party = {
   guests: [],
 
-  addGuest() {}
+  addGuest(event) {
+    event.preventDefault();
+    const inputs = document.querySelectorAll("input");
+    [first, last, email, item, plusOne, plusOneName] = inputs;
+    const guest = {
+      firstName: first.value,
+      lastName: last.value,
+      email: email.value,
+      bringItem: item.value,
+      plusOne: plusOneName.value
+    };
+    if (!!guest.firstName && !!guest.lastName && !!guest.bringItem) {
+      this.guests.push(guest);
+    }
+  }
 };
 
 const ui = {
