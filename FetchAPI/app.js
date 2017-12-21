29dc1a5ca9cc4d5b80a72f7f4d3a84ba7d0f6c40
PatrickSchuelke <p-schuelke@gmx.de>
2017-12-21 19:11:40 +0100

    linted with airbnb

--- FetchAPI/app.js
+++ FetchAPI/app.js
@@ -1,28 +1,28 @@
-const container = document.querySelector(".container");
-
-// fetch chuck norris jokes
-function fetchChuckJokes(amount = 1) {
-  fetch(`http://api.icndb.com/jokes/random/${amount}/`)
-    .then(response => response.json())
-    .then(data => displayList(data.value))
-    .catch(err => console.log("There was an error: " + err));
-}
+const container = document.querySelector('.container');
 
 // expects array of JSON objects to create and display joke list
 function displayList(jokes) {
-  const ul = document.createElement("ul");
-  jokes.forEach(joke => {
-    const li = document.createElement("li");
+  const ul = document.createElement('ul');
+  jokes.forEach((joke) => {
+    const li = document.createElement('li');
     li.innerHTML = joke.joke;
     ul.appendChild(li);
   });
   container.appendChild(ul);
 }
 
+// fetch chuck norris jokes
+function fetchChuckJokes(amount = 1) {
+  fetch(`http://api.icndb.com/jokes/random/${amount}/`)
+    .then(response => response.json())
+    .then(data => displayList(data.value))
+    .catch(err => console.log(`There was an error: ${err}`));
+}
+
 // handle button click
 function handleClick() {
-  const category = document.querySelector("select").value;
-  const amountJokes = document.querySelector("#amountJokes").value;
+  const category = document.querySelector('select').value;
+  const amountJokes = document.querySelector('#amountJokes').value;
 
-  if (category === "chuckN") fetchChuckJokes(amountJokes);
+  if (category === 'chuckN') fetchChuckJokes(amountJokes);
 }
