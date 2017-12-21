f79940865d82ae67c45e7dbbbd7e8ec8c223b002
PatrickSchuelke <p-schuelke@gmx.de>
2017-12-21 19:56:11 +0100

    linted with airbnb rules

--- TodoList/todo.js
+++ TodoList/todo.js
@@ -1,124 +1,129 @@
-var todoList = {
+const todoList = {
   todos: [],
-  //Function to add ToDo Items as objects
-  addTodo: function(todoText) {
+  // Function to add ToDo Items as objects
+  addTodo(todoText) {
     this.todos.push({
-      todoText: todoText,
+      todoText,
       completed: false
     });
   },
-  //Function to change ToDo Items
-  changeTodo: function(position, todoText) {
+  // Function to change ToDo Items
+  changeTodo(position, todoText) {
     this.todos[position].todoText = todoText;
   },
-  //Function to delete ToDo Items
-  deleteTodo: function(position) {
+  // Function to delete ToDo Items
+  deleteTodo(position) {
     this.todos.splice(position, 1);
   },
-  //Function to toggle completed Status
-  toggleCompleted: function(position) {
-    let todo = this.todos[position];
+  // Function to toggle completed Status
+  toggleCompleted(position) {
+    const todo = this.todos[position];
     todo.completed = !todo.completed;
   },
-  //Function to toggle completed status of ALL items
-  toggleAll: function() {
-    let totalTodos = this.todos.length;
+  // Function to toggle completed status of ALL items
+  toggleAll() {
+    const totalTodos = this.todos.length;
     let completedTodos = 0;
     // Check and store if there are completed Items
-    for (let todo of this.todos) {
+    this.todos.forEach((todo) => {
       if (todo.completed === true) {
-        completedTodos++;
+        completedTodos += 1;
       }
-    };
+    });
 
-    for (let todo of this.todos) {
+
+    this.todos.forEach((todo) => {
+      const todoItem = todo;
       // Toggle all to false if everything is true
       if (completedTodos === totalTodos) {
-        todo.completed = false;
+        todoItem.completed = false;
+        // Otherwiske make everything true
+      } else {
+        todoItem.completed = true;
       }
-      // Otherwiske make everything true
-      else {
-        todo.completed = true;
-      }
-    };
+    });
   }
 };
 
-// Object for button onclick events
-let handlers = {
-  //Add textfield value to todo list and clear textfield
-  addTodo: function() {
-    let addTodoInput = document.getElementById('addTodoInput');
-    todoList.addTodo(addTodoInput.value);
-    addTodoInput.value = "";
-    view.displayTodos();
-  },
-  //Changes ToDo item at user specified position and clears inputfields
-  changeTodo: function() {
-    let changeTodoPosition = document.getElementById('changeTodoPosition');
-    let changeTodoText = document.getElementById('changeTodoText');
-    todoList.changeTodo(changeTodoPosition.valueAsNumber, changeTodoText.value);
-    changeTodoPosition.value = "";
-    changeTodoText.value = "";
-    view.displayTodos();
-  },
-  //Delete ToDo Item at user specified location and clear inputfield
-  deleteTodo: function(position) {
-    todoList.deleteTodo(position);
-    view.displayTodos();
-  },
-  //Delete ToDo Item at user specified location and clear inputfield
-  toggleCompleted : function() {
-    let toggleCompletedPosition = document.getElementById('toggleCompletedPosition');
-    todoList.toggleCompleted(toggleCompletedPosition.valueAsNumber);
-    toggleCompletedPosition.value = "";
-    view.displayTodos();
-  },
-  //ToggleAll completed on Buttonclick
-  toggleAll: function() {
-    todoList.toggleAll();
-    view.displayTodos();
-  }
 
-}
-
-//Object for everything UI-only related
-let view = {
-  displayTodos: function() {
-    let todoUl = document.querySelector('ul');
-    todoUl.innerHTML = "";
-    //Create an li-element for every todo item
-    todoList.todos.forEach(function(todo, position) {
-      let todoTextWithCompletion = "";
-      let todoLi = document.createElement('li');
+// Object for everything UI-only related
+const view = {
+  displayTodos() {
+    const todoUl = document.querySelector('ul');
+    todoUl.innerHTML = '';
+    // Create an li-element for every todo item
+    todoList.todos.forEach((todo, position) => {
+      let todoTextWithCompletion = '';
+      const todoLi = document.createElement('li');
 
       if (todo.completed === true) {
-        todoTextWithCompletion = "(x) " + todo.todoText
-      }
-      else {
-        todoTextWithCompletion = "() " + todo.todoText
+        todoTextWithCompletion = `(x) ${todo.todoText}`;
+      } else {
+        todoTextWithCompletion = `() ${todo.todoText}`;
       }
       todoLi.id = position;
       todoLi.textContent = todoTextWithCompletion;
       todoLi.appendChild(this.createDeleteButton());
       todoUl.appendChild(todoLi);
-    }, this); // the 'this' is optional argument in forEach function and ensures that the 'this' correctly points at the object "view"
+      // the "this" is optional argument in forEach function and ensures that the
+      // 'this' correctly points at the object "view"
+    }, this);
   },
-  createDeleteButton: function() {
-    let deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-    deleteButton.className = "deleteButton";
+  createDeleteButton() {
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'deleteButton';
     return deleteButton;
   },
-  setUpEventListeners: function() {
-    let todosUl = document.querySelector('ul');
-    todosUl.addEventListener("click", function(event) {
-      let elementClicked = event.target;
-      if (elementClicked.className === "deleteButton") {
-        handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
+  setUpEventListeners() {
+    const todosUl = document.querySelector('ul');
+    todosUl.addEventListener('click', (event) => {
+      const elementClicked = event.target;
+      if (elementClicked.className === 'deleteButton') {
+        handlers.deleteTodo(Number(elementClicked.parentNode.id));
       }
     });
   }
 };
+
+// Object for button onclick events
+const handlers = {
+  // Add textfield value to todo list and clear textfield
+  addTodo() {
+    const addTodoInput = document.getElementById('addTodoInput');
+    todoList.addTodo(addTodoInput.value);
+    addTodoInput.value = '';
+    view.displayTodos();
+  },
+  // Changes ToDo item at user specified position and clears inputfields
+  changeTodo() {
+    const changeTodoPosition = document.getElementById('changeTodoPosition');
+    const changeTodoText = document.getElementById('changeTodoText');
+    todoList.changeTodo(changeTodoPosition.valueAsNumber, changeTodoText.value);
+    changeTodoPosition.value = '';
+    changeTodoText.value = '';
+    view.displayTodos();
+  },
+  // Delete ToDo Item at user specified location and clear inputfield
+  deleteTodo(position) {
+    todoList.deleteTodo(position);
+    view.displayTodos();
+  },
+  // Delete ToDo Item at user specified location and clear inputfield
+  toggleCompleted() {
+    const toggleCompletedPosition = document.getElementById('toggleCompletedPosition');
+    todoList.toggleCompleted(toggleCompletedPosition.valueAsNumber);
+    toggleCompletedPosition.value = '';
+    view.displayTodos();
+  },
+  // ToggleAll completed on Buttonclick
+  toggleAll() {
+    todoList.toggleAll();
+    view.displayTodos();
+  }
+
+};
+
+// ------ code execution ------
 
 view.setUpEventListeners();
