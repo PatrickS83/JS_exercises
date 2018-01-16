b70cb657c089102a1e980dbdcc8c1a9d9ab50e66
Patrick Schuelke <p-schuelke@gmx.de>
2018-01-16 11:36:42 +0100

    refactored into objects

--- calculator/calc.js
+++ calculator/calc.js
@@ -1,17 +1,49 @@
 const calcButtons = document.querySelectorAll('[data-value');
 const calcDisplay = document.querySelector('.calc_display');
-const input1 = '';
-const input2 = '';
 
+// calculator object handles the logic
+const calculator = {
+  values: [],
+  // add numbers or operators to values array
+  addValue(value) {
+    calculator.values.push(value);
+  },
+
+  solveEquation() {
+    // convert array to string equation and remove "=" at the end
+    const joinedValues = this.values.join('').slice(0, -1);
+    // display result in calculator display
+    const result = eval(joinedValues);
+    ui.displayResult(result);
+    this.values = [result];
+  },
+  resetCalc() {
+    this.values = [];
+    ui.displayResult('');
+  }
+
+};
+
+// ui object handles the user interface
+const ui = {
+  displayResult(result, concat) {
+    if (concat) calcDisplay.innerHTML += result;
+    else calcDisplay.innerHTML = result;
+  }
+};
+
+// set up event listener for calculator buttons
 function setupEventListeners() {
   calcButtons.forEach(button => button.addEventListener('click', handleClick));
 }
 
 function handleClick() {
-  calcDisplay.innerHTML = this.dataset.value;
+  calculator.addValue(this.dataset.value);
+  ui.displayResult(this.dataset.value, true);
+  // on "=" press the equation is solved
+  if (this.dataset.value === '=') calculator.solveEquation();
+  if (this.dataset.value === 'C') calculator.resetCalc();
 }
 
-
-// executed code
-
+// ############ executed code ##############
 setupEventListeners();
