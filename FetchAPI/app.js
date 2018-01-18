ea940bedfdcc3ec77c1454d2797a32bc8377a215
Patrick Schuelke <p-schuelke@gmx.de>
2018-01-18 11:52:20 +0100

    add numberfacts and dynamic UI

--- FetchAPI/app.js
+++ FetchAPI/app.js
@@ -1,5 +1,8 @@
 const container = document.querySelector('.container');
 const fetchBtn = document.querySelector('.fetchBtn');
+const apiSelect = document.querySelector('.api_select');
+const labelAmount = document.querySelector('#labelAmount');
+const inputAmount = document.querySelector('#amountJokes');
 
 const jokeFetcher = {
   // fetch chuck norris jokes
@@ -20,6 +23,12 @@ const jokeFetcher = {
       .then(data => ui.displayList(data))
       .catch(err => console.log(`There was an error: ${err}`));
   },
+  fetchNumFact(number = 1) {
+    fetch(`http://numbersapi.com/${number}/`)
+      .then(response => response.text())
+      .then(data => ui.displayList([data]))
+      .catch(err => console.log(`There was an error: ${err}`));
+  },
 };
 
 const ui = {
@@ -35,7 +44,26 @@ const ui = {
       li.innerHTML = joke;
       ul.appendChild(li);
     });
+    // display final list in browserwindow
     container.appendChild(ul);
+  },
+  // expects the clicked API selector and changes the UI accordingly
+  changeDescription(selectedAPI) {
+    let placeHold;
+    if (selectedAPI === 'chuckN') placeHold = 'Jokes';
+    else if (selectedAPI === 'ronSwan') placeHold = 'Quotes';
+    // block if only one word has to change
+    if (placeHold) {
+      labelAmount.innerHTML = `How many ${placeHold}?`;
+      fetchBtn.innerHTML = `Fetch ${placeHold}`;
+      inputAmount.setAttribute('placeholder', `Amount of ${placeHold}`);
+
+      // block if more than one word has to be changed
+    } else if (selectedAPI === 'numFact') {
+      labelAmount.innerHTML = 'Fact about which number?';
+      fetchBtn.innerHTML = 'Fetch fact';
+      inputAmount.setAttribute('placeholder', 'Fact about which number?');
+    }
   }
 };
 
@@ -43,6 +71,7 @@ const controller = {
   // setup Eventlistener
   setUpEventListeners() {
     fetchBtn.addEventListener('click', this.handleBtnClick);
+    apiSelect.addEventListener('click', this.handleSelectClick);
   },
   // handle button click
   handleBtnClick() {
@@ -50,6 +79,12 @@ const controller = {
     const amountJokes = document.querySelector('#amountJokes').value;
     if (category === 'chuckN') jokeFetcher.fetchChuckJokes(amountJokes);
     if (category === 'ronSwan') jokeFetcher.fetchRonQuotes(amountJokes);
+    if (category === 'numFact') jokeFetcher.fetchNumFact(amountJokes);
+  },
+  // handle clicks on the API selector
+  handleSelectClick(e) {
+    const elementClicked = e.target;
+    ui.changeDescription(elementClicked.value);
   }
 };
 
