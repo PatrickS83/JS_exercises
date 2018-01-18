43d43f9fc2e5f7d8fcb615d0a9ce0b9eed9cfd82
Patrick Schuelke <p-schuelke@gmx.de>
2018-01-18 09:38:58 +0100

    refactored funtions into methods

--- FetchAPI/app.js
+++ FetchAPI/app.js
@@ -1,28 +1,41 @@
 const container = document.querySelector('.container');
+const fetchBtn = document.querySelector('.fetchBtn');
 
-// expects array of JSON objects to create and display joke list
-function displayList(jokes) {
-  const ul = document.createElement('ul');
-  jokes.forEach((joke) => {
-    const li = document.createElement('li');
-    li.innerHTML = joke.joke;
-    ul.appendChild(li);
-  });
-  container.appendChild(ul);
-}
+const jokeFetcher = {
+  // fetch chuck norris jokes
+  fetchChuckJokes(amount = 1) {
+    fetch(`http://api.icndb.com/jokes/random/${amount}/`)
+      .then(response => response.json())
+      .then(data => ui.displayList(data.value))
+      .catch(err => console.log(`There was an error: ${err}`));
+  }
+};
 
-// fetch chuck norris jokes
-function fetchChuckJokes(amount = 1) {
-  fetch(`http://api.icndb.com/jokes/random/${amount}/`)
-    .then(response => response.json())
-    .then(data => displayList(data.value))
-    .catch(err => console.log(`There was an error: ${err}`));
-}
+const ui = {
+  // expects array of JSON objects to create and display joke list
+  displayList(jokes) {
+    const ul = document.createElement('ul');
+    jokes.forEach((joke) => {
+      const li = document.createElement('li');
+      li.innerHTML = joke.joke;
+      ul.appendChild(li);
+    });
+    container.appendChild(ul);
+  }
+};
 
-// handle button click
-function handleClick() {
-  const category = document.querySelector('select').value;
-  const amountJokes = document.querySelector('#amountJokes').value;
+const controller = {
+  // setup Eventlistener
+  setUpEventListeners() {
+    fetchBtn.addEventListener('click', this.handleClick);
+  },
+  // handle button click
+  handleClick() {
+    const category = document.querySelector('select').value;
+    const amountJokes = document.querySelector('#amountJokes').value;
+    if (category === 'chuckN') jokeFetcher.fetchChuckJokes(amountJokes);
+  }
+};
 
-  if (category === 'chuckN') fetchChuckJokes(amountJokes);
-}
+// ######## executed code ##################
+controller.setUpEventListeners();
