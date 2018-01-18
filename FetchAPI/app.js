063e7da7b101ba88cd7513b4402f1870a110a6f1
Patrick Schuelke <p-schuelke@gmx.de>
2018-01-18 10:12:06 +0100

    added functionality for Ron Swanson quotes

--- FetchAPI/app.js
+++ FetchAPI/app.js
@@ -6,13 +6,24 @@ const jokeFetcher = {
   fetchChuckJokes(amount = 1) {
     fetch(`http://api.icndb.com/jokes/random/${amount}/`)
       .then(response => response.json())
-      .then(data => ui.displayList(data.value))
+      .then((data) => {
+        // convert array of json objects to array of jokes as strings
+        const jokeArr = data.value.map(joke => joke.joke);
+        ui.displayList(jokeArr);
+      })
       .catch(err => console.log(`There was an error: ${err}`));
-  }
+  },
+  // fetch ron swanson quotes
+  fetchRonQuotes(amount = 1) {
+    fetch(`http://ron-swanson-quotes.herokuapp.com/v2/quotes/${amount}/`)
+      .then(response => response.json())
+      .then(data => ui.displayList(data))
+      .catch(err => console.log(`There was an error: ${err}`));
+  },
 };
 
 const ui = {
-  // expects array of JSON objects to create and displays joke list
+  // expects array of strings to create and displays joke list
   displayList(jokes) {
     // clear list if there is already a list
     if (document.querySelector('ul')) document.querySelector('ul').remove();
@@ -21,7 +32,7 @@ const ui = {
     // create a new list item for each joke and append to list
     jokes.forEach((joke) => {
       const li = document.createElement('li');
-      li.innerHTML = joke.joke;
+      li.innerHTML = joke;
       ul.appendChild(li);
     });
     container.appendChild(ul);
@@ -38,6 +49,7 @@ const controller = {
     const category = document.querySelector('select').value;
     const amountJokes = document.querySelector('#amountJokes').value;
     if (category === 'chuckN') jokeFetcher.fetchChuckJokes(amountJokes);
+    if (category === 'ronSwan') jokeFetcher.fetchRonQuotes(amountJokes);
   }
 };
 
